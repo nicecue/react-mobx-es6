b0edec5b91ac40a6ab890ed6575a2051c8c2159d
정용남 <es-youngnjung@smilegate.com>
2018-10-25 17:51:48 +0900

    carousel 구현중

--- src/routes/carousel-test/components/Carousel.jsx
+++ src/routes/carousel-test/components/Carousel.jsx
@@ -1,76 +1,93 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import saveRefs from 'react-save-refs';
 
 import './Carousel.css';
 
-const listData = [
-  {
-    name: 'aaaa',
-    age: 18
-  },
-  {
-    name: 'aaaa',
-    age: 18
-  },
-  {
-    name: 'aaaa',
-    age: 18
-  },
-  {
-    name: 'aaaa',
-    age: 18
-  },
-  {
-    name: 'aaaa',
-    age: 18
-  },
-  {
-    name: 'aaaa',
-    age: 18
-  },
-  {
-    name: 'aaaa',
-    age: 18
-  },
-  {
-    name: 'aaaa',
-    age: 18
-  }
-]
-
-
 class Carousel extends Component {
+  static defaultProps = {
+    animateDuration: 300,
+    maxShowItem: 4
+  }
+
   constructor(props) {
     super(props);
 
     this.state = {
       currentIdx: -1
     };
+
+    this.items = new Map();
+    window.items = this.items;
+  }
+
+  componentDidMount() {
+    const childs = React.Children.toArray(this.props.children);
+    if (childs.length) {
+      this.setState({currentIdx: 0});
+    }
   }
 
   rollUp = () => {
-    
+    const { currentIdx } = this.state;
+    const currentChild = ReactDOM.findDOMNode(this.items.get(currentIdx));
   }
 
   rollDown = () => {
 
   }
 
-  renderList = () => {
-    return listData.map((user, idx) => {
-      return (
-        <div className="user" key={idx}>
-          <span>{user.name}</span>:&nbsp;
-          <span>{user.age}세</span>
-        </div>
-      )
-    })
-  }
-
   render() {
-    const list = this.renderList();
+    const {
+      className,
+      children,
+      maxShowItem
+    } = this.props;
+    const { currentIdx } = this.state;
+
+    if (currentIdx < 0) {
+      return null;
+    }
+
+    const childs = React.Children.toArray(children);
+    const lastIdx = childs.length - 1;
+
+    const viewStart = currentIdx;
+    let viewEnd = viewStart + maxShowItem;
+    let fakeViewListTailCnt = 0;
+    
+    if (viewEnd > lastIdx) {
+      viewEnd = lastIdx;
+      fakeViewListTailCnt = viewEnd - lastIdx;
+    } else {
+      childs.slice(viewEnd + 1,)
+    }
+
+    console.error(`${viewStart} - ${viewEnd}`);
+
+    const viewList = childs.slice(viewStart, viewEnd).map((child, idx) => {
+      return React.cloneElement(child, {});
+    })
+    if (fakeViewListTailCnt) {
+      const viewListTail = childs.slice(0, fakeViewListTailCnt).map((child, idx) => {
+        return React.cloneElement(child, {});
+      });
+      viewList.push(viewListTail);
+    }
+
+    const tailIdx = fakeViewListTailCnt? fakeViewListTailCnt: viewEnd + 1;
+
+    const tail = React.cloneElement(childs[tailIdx], {});
+
+    const fakeChildren = [];
+    fakeChildren.push(viewList);
+    fakeChildren.push(tail);
+    
     return (
-      <div className="container">
-        { list }
+      <div className={`carousel-wrapper${className? ` ${className}`: ''}`}>
+        <div className="carousel-container" ref={r=>this.container=r}>
+          { fakeChildren }
+        </div>
       </div>
     );
   }
--- src/routes/carousel-test/index.js
+++ src/routes/carousel-test/index.js
@@ -1,3 +1,87 @@
+import React, { Component } from 'react';
 import { Carousel } from './components';
 
-export default Carousel;
+const list = [
+  {
+    name: 'aaaa',
+    age: 8
+  },
+  {
+    name: 'bbbb',
+    age: 10
+  },
+  {
+    name: 'cccc',
+    age: 19
+  },
+  {
+    name: 'dddd',
+    age: 22
+  },
+  {
+    name: 'eeee',
+    age: 38
+  },
+  {
+    name: 'ffff',
+    age: 42
+  },
+  {
+    name: 'gggg',
+    age: 13
+  },
+  {
+    name: 'hhhh',
+    age: 5
+  }
+]
+
+class User extends Component {
+  render() {
+    const {
+      name,
+      age
+    } = this.props;
+    return (
+      <div className="user">
+        <span>{name}</span>:&nbsp;
+        <span>{age} years old</span>
+      </div>
+    );
+  }
+}
+
+class CarouselPage extends Component {
+  onBtnUp = () => {
+    if (this.carousel) {
+      this.carousel.rollUp();
+    }
+  }
+
+  onBtnDown = () => {
+    if (this.carousel) {
+      this.carousel.rollDown();
+    }
+  }
+
+  renderUserList = () => {
+    return list.map((user, idx) => {
+      return <User name={user.name} age={user.age} key={idx} />
+    });
+  }
+
+  render() {
+    const userList = this.renderUserList();
+    return (
+      <div>
+        <Carousel ref={r=>this.carousel=r}>
+          { userList }
+        </Carousel>
+        <button onClick={this.onBtnUp}>UP</button>
+        <button onClick={this.onBtnDown}>DOWN</button>
+      </div>
+    );
+  }
+}
+
+export default CarouselPage;
